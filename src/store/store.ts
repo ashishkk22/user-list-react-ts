71442e18dc33bfa3489f2ff44a6874c2157a00b2
ashishkk22 <ashishkachhadiya22@gmail.com>
2023-04-11 16:10:33 +0530

    added the mode in .env to display redux devTools

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,12 +4,14 @@ import type { TypedUseSelectorHook } from "react-redux";
 import personReducer from "./features/personSlice";
 import { userApi } from "../api/user/userApis";
 
+const environment = import.meta.env;
+
 export const store = configureStore({
   reducer: {
     person: personReducer,
     [userApi.reducerPath]: userApi.reducer,
   },
-  devTools: true,
+  devTools: environment.VITE_APP_MODE === "development",
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(userApi.middleware),
 });
